129795974dffe21e82bec289332ea9fff73964bd
android <@gmail.com>
2026-03-27 21:19:11 -0300

    git

--- app/src/main/java/com/matheus/modularappdelivery/LoginActivity.java
+++ app/src/main/java/com/matheus/modularappdelivery/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.matheus.modularappdelivery;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,47 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText email, senha;
+    TextView erro,criarConta;
+
+    Button entrar;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        initComponents();
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void initComponents() {
+        email = findViewById(R.id.email);
+        senha = findViewById(R.id.senha);
+        erro = findViewById(R.id.mensagemErro);
+        criarConta = findViewById(R.id.criarConta);
+        entrar = findViewById(R.id.btnEntrar);
+
+
+
     }
 }
